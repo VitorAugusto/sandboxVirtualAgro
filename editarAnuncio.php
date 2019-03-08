3a9ea88933994024686d0124f5bfcb69231d37f5
VitorAugusto <vitoraugusto2508@gmail.com>
2019-03-07 22:25:01 -0300

    ARQUIVOS PHP VIRTUAL AGRO - Arquivos adicionais
    Foram adicionados os arquivos do VIRTUAL AGRO que está na minha máquina.

---
+++ editarAnuncio.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+include('tools.php');
+
+if(isset($_SESSION['id'])){ //SE ESTIVER LOGADO
+
+
+if(meuAnuncio(getIdAnuncianteIdAnuncio($_GET['idAnuncio']), $_SESSION['id'])){
+	ECHO "ESSE ANÚNCIO É SEU";
+}else{
+	ECHO "ESSE ANÚNCIO NÃO TE PERTENCE";
+}
+
+
+} else{
+
+	ECHO "OPERAÇÃO INVÁLIDA";
+}
+?>
