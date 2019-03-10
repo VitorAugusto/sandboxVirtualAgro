276e17a7aeb04354a221f59a93bd32d393429f99
MuriloGois <murilogois59@gmail.com>
2019-03-10 19:12:22 -0300

    login
    .css do login 95% concluida.

--- home.php
+++ home.php
@@ -4,9 +4,10 @@
         <title>HOME - Virtual Agro</title>
         <meta charset="UTF-8">
         <link rel="icon" type="image/png" sizes="64x64" href="imagens/logo/virtual-agro-logo-png.png">
-        <link href="css/style.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="css/style.css" rel="stylesheet">
     </head>
     <body>
         <header class="navigation">
@@ -17,62 +18,58 @@
                         <span class="text-menu">MENU</span>
                     </button>
                 </div>
-                <img class="logo-header" src="imagens/logo/virtual-agro-logo-png.png">
+                <img class="logo-header" src="imagens/logo/virtual-agro-logo-nome.png">
                 <div class="right-side">                                        
                 </div>                
             </div>            
         </header>
-        <section id="conteudo">
-                <div class="container fold-cover">
-                    <div class="clearfix">
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                        <h1 class="search-title">
-                            PRODUTOS ORGÂNICOS<br>
-                            <span>LIVRES DE AGROTÓXICOS</span>
-                        </h1>
-                    </div>
-                    <div class="search-content">
-                        <form method="GET" action="" accept-charset="UTF-8" class="" id="" name="">
-                            <p class="search-label description">Com o Virtual Agro você pode conferir os números de telefone e whatsapp de agricultores de sua região. Com isso oferecemos uma opção pra quem deseja consumir produtos 100% orgânicos e sem agrotóxicos. Confira!
-                            </p>
-                            <br>
-                            <div class="search-label">O que você procura?</div>
-                            <div class="linha">
-                                <div class="check-box">
-                                    <input id="" class="" type="checkbox" name="" value="" alt="Frutas" checked>
-                                    <label for="">
-                                        <span class="icon-menu"><span class="icon"></span></span>
-                                        <span class="icon-text">FRUTAS</span>
-                                    </label>
-                                </div>                    
-                                <div class="check-box">
-                                    <input id="" class="" type="checkbox" name="" value="" alt="Legumes">
-                                    <label for="">
-                                        <span class="icon-menu"><span class="icon"></span></span>
-                                        <span class="icon-text">LEGUMES</span>
-                                    </label>
+        <div class="all">
+            <section class="conteudo">
+                        <div class="clearfix">
+            
+                            <h1 class="search-title">
+                                PRODUTOS ORGÂNICOS<br>
+                                <span>LIVRES DE AGROTÓXICOS</span>
+                            </h1>
+                        </div>
+                        <div class="search-content">
+                            <form method="GET" action="" accept-charset="UTF-8" class="" id="" name="">
+                                <p class="search-label description">Com o Virtual Agro você pode conferir os números de telefone e whatsapp de agricultores de sua região. Com isso oferecemos uma opção pra quem deseja consumir produtos 100% orgânicos e sem agrotóxicos. Confira!
+                                </p>
+                                <br>
+                                <div class="search-label">O que você procura?</div>
+                                <div class="linha">
+                                    <div class="check-box">
+                                        <input id="" class="" type="checkbox" name="" value="" alt="Frutas" checked>
+                                        <label for="">
+                                            <span class="icon-menu"><span class="icon"></span></span>
+                                            <span class="icon-text">FRUTAS</span>
+                                        </label>
+                                    </div>                    
+                                    <div class="check-box">
+                                        <input id="" class="" type="checkbox" name="" value="" alt="Legumes">
+                                        <label for="">
+                                            <span class="icon-menu"><span class="icon"></span></span>
+                                            <span class="icon-text">LEGUMES</span>
+                                        </label>
+                                    </div>
+                                    <div class="check-box">
+                                        <input id="" class="" type="checkbox" name="" value="" alt="Verduras">
+                                        <label for="">
+                                            <span class="icon-menu"><span class="icon"></span></span>
+                                            <span class="icon-text">VERDURAS</span>
+                                        </label>
+                                    </div>
                                 </div>
-                                <div class="check-box">
-                                    <input id="" class="" type="checkbox" name="" value="" alt="Verduras">
-                                    <label for="">
-                                        <span class="icon-menu"><span class="icon"></span></span>
-                                        <span class="icon-text">VERDURAS</span>
-                                    </label>
-                                </div>
-                            </div>
-                            <button class="buscar" type="submit">
-                                <span class="icon-text">BUSCAR</span>
-                                <span class="icon-menu"><i class="fa fa-chevron-circle-right"></i></span>
-                            </button>
-                        </form>
+                                <button class="buscar" type="submit">
+                                    <span class="icon-text">BUSCAR</span>
+                                    <span class="icon-menu"><i class="fa fa-chevron-circle-right"></i></span>
+                                </button>
+                            </form>
                     </div>
-                </div>
-        </section>
+            </section>
+        </div>
+       
         <footer>
             <div class="container">
                 <a class="brand" href="#">
--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 			<title>LOGIN - Virtual Agro</title>
 			<meta charset="UTF-8">
 			<link rel="icon" type="image/png" sizes="64x64" href="imagens/logo/virtual-agro-logo-png.png">
-			<link href="css/style.css" rel="stylesheet">
 			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+			<link href="css/style.css" rel="stylesheet">
 	</head>
 	<body>
 		<header class="navigation">
@@ -18,7 +18,7 @@
 						<span class="text-menu">MENU</span>
 					</button>
 				</div>
-				<img class="logo-header" src="imagens/logo/virtual-agro-logo-png.png">
+				<img class="logo-header" src="imagens/logo/virtual-agro-logo-nome.png">
 				<div class="right-side">                                        
 				</div>                
 			</div>            
@@ -36,39 +36,43 @@
 
 				?>
 				
-					<form action="accessManager.php?tipoOperacao=login" method="post">				
-						<label> LOGIN </label>
-						<input type="text" name="campoUsername">
-						<br>
-						<br>
-						<label>SENHA</label>
-						<input type="password" name="campoSenha">
-						<br>
-						<br>
-						<button class="buscar" type="submit">
-                	    	<span class="icon-text">ENVIAR</span>
-            	        	<span class="icon-menu"><i class="fa fa-chevron-circle-right"></i></span>
-        	        	</button>
-					</form>
+				<form action="accessManager.php?tipoOperacao=login" method="post">	
+					<div class="login-senha">
+						<input type="text" name="campoUsername" placeholder="LOGIN">
+						<button class="login" type="submit">
+							<i class="fas fa-user"></i>
+						</button>
+					</div>			
+					<div class="login-senha">
+						<input type="password" name="campoSenha" placeholder="SENHA">
+						<button class="login" type="submit">
+							<i class="fas fa-key"></i>
+						</button>
+					</div>						
+					<button class="buscar" type="submit">
+                		<span class="icon-text">ENVIAR</span>
+            	        <span class="icon-menu"><i class="fa fa-chevron-circle-right"></i></span>
+        	    	</button>
+				</form>
 
-				<h1>Não é cadastrado ? </h1>
+				<h3 style="color: #1b6e5c;">Não é cadastrado?</h3>
 				<a href="cadastro.php">
-					CADASTRO
+					<b>CADASTRO</b>
 				</a>
 
-				<H2> QUER VER TODOS OS ANÚNCIOS ? </H2>
+				<h4 style="color: #1b6e5c;"> QUER VER TODOS OS ANÚNCIOS?</h4>
 				<a href="allAnuncios.php">
-					TODOS OS ANÚNCIOS
+					<b>TODOS OS ANÚNCIOS</b>
 				</a>
 			</section>
 		</div>		               
 		<footer>
 			<div>
 				<a class="brand" href="#">
-				<img class="logo" src="./imagens/logo/virtual-agro-logo-png.png" alt="">            
+					<img class="logo" src="./imagens/logo/virtual-agro-logo-png.png" alt="">            
 				</a>
 				<hr>
-				<div class="copyright">Copyright 2019 © <a href="#">Virtual Agro</a>.</div>
+				<div class="copyright">Copyright 2019 © <a href="#"><b>Virtual Agro</b></a>.</div>
 			</div>
 		</footer>
 	</body>
