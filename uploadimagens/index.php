c0efe5920e592c2c2eddde0756ca7ac6839f0511
VitorAugusto <vitoraugusto2508@gmail.com>
2019-03-10 22:40:01 -0300

    Página pra POPULAR O BANCO
    segue a página pra popular o banco
    
    entra nela pelo sandboxVirtualAgro/uploadimagens

---
+++ uploadimagens/index.php
@@ -0,0 +1,97 @@
+<?php
+
+if(isset($_POST['botao'])){
+
+	$categoria = $_POST['categoria'];
+
+
+$db_host = 'localhost'; //endereço
+$db_database = 'virtualagro'; //banco de dados
+$db_username = 'root';//usuário
+$db_password = ''; //senha
+
+$minhaConexao = mysqli_connect($db_host,$db_username,$db_password,$db_database);
+
+for ($i=0; $i < sizeof($_FILES['image']['name']); $i++) { 
+
+
+	$target = "../"."imagens/". $categoria ."/". basename($_FILES['image']['name'][$i]); // PRA ONDE VAI AS IMAGENS
+
+	$imagem = $categoria ."/". $_FILES['image']['name'][$i];
+
+	$nomeproduto = pathinfo($_FILES['image']['name'][$i], PATHINFO_FILENAME);
+
+
+	$comandoSQL = "INSERT INTO produtos (nome,categoria,imagemprincipal) VALUES ('$nomeproduto','$categoria','$imagem')";
+
+	mysqli_query($minhaConexao,$comandoSQL);
+
+	if(move_uploaded_file($_FILES['image']['tmp_name'][$i], $target)){
+
+		$msg = "SUCESSO";
+		echo "<br>";
+	}else{
+		$msg = "ERRO";
+		echo "<br>";
+	}
+
+	echo $msg;
+
+}
+
+
+
+	// $imagem = $categoria ."/". $_FILES['image']['name'];
+
+	// $nomeproduto = pathinfo($_FILES['image']['name'], PATHINFO_FILENAME);
+
+
+	// $comandoSQL = "INSERT INTO produtos (nome,categoria,imagemprincipal) VALUES ('$nomeproduto','$categoria','$imagem')";
+
+	// mysqli_query($minhaConexao,$comandoSQL);
+
+	// if(move_uploaded_file($_FILES['image']['tmp_name'], $target)){
+
+	// 	$msg = "SUCESSO";
+	// }else{
+	// 	$msg = "ERRO";
+	// }
+
+
+
+
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>
+		Image Upload
+
+	</title>
+</head>
+<body>
+
+	<form method="post" action="index.php" enctype="multipart/form-data">
+
+		<input type="file" name="image[]" required="" multiple="multiple">
+
+		<br>
+
+		<select name="categoria">
+			<option value="fruta">FRUTA</option>
+			<option value="verdura">VERDURA</option>
+			<option value="legume">LEGUME</option>
+			<option value="tempero">TEMPERO</option>
+			<option value="outro">OUTRO</option>
+		</select>
+		<br>
+	    <input type="submit" name="botao">
+	</form>
+
+</body>
+</html>
