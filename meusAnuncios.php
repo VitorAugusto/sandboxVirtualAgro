00cb18caec02fe953b17aeeb13d6cf2e33814a49
MuriloGois <murilogois59@gmail.com>
2019-04-08 01:00:41 -0300

    Barcelona

--- meusAnuncios.php
+++ meusAnuncios.php
@@ -56,37 +56,32 @@
 						$displayMeusAnuncios = mysqli_query($GLOBALS['dao'],$selectMeusAnuncios);
 
 						while($coluna = mysqli_fetch_array($displayMeusAnuncios)) {
-							echo "<div class='container'>";	 //
-							echo "<div class='row'>";	 //
+							echo "<div class='meuAnuncio'>";	 //
 							$comandoGetIdProduto = "SELECT id_produto FROM anuncios WHERE id = '$coluna[id]'";  //ESSE MÓDULO AQUI ADICIONA AS IMAGENS NO ALL ANUNCIOS
 							$display = mysqli_query($GLOBALS['dao'], $comandoGetIdProduto);
-							echo "<div class='col-sm'>"; //
+							echo "<div class='borda'>";
+							echo "<div >"; //
 							echo getImagemProduto(mysqli_fetch_row($display)[0]);
 							echo "</div>"; //
-							echo "<div class='col-sm'>"; //
-							echo "ID DO ANÚNCIO: " . $coluna['id'] ;
 							echo "</div>"; //
-							echo "<div class='col-sm'>"; //
-							echo "ID DO ANUNCIANTE: " . $coluna['id_anunciante'];
+							echo "<h3>"; //
+							echo getNomeProdutoPeloID($coluna['id']);
+							echo "</h3>";
+							echo "<div class='info'>"; //
+							echo "<b>CATEGORIA:</b>" . $coluna['categoria'] ;
 							echo "</div>"; //
-							echo "<div class='col-sm'>"; //
-							echo "CATEGORIA: " . $coluna['categoria'] ;
+							echo "<div class='info'>"; //
+							echo "<b>TEXTO DO ANÚNCIO:</b>" . $coluna['texto_anuncio'];
 							echo "</div>"; //
-							echo "<div class='col-sm'>"; //
-							echo "TEXTO DO ANÚNCIO: " . $coluna['texto_anuncio'];
-							echo "</div>"; //
-							echo "<div class='col-sm'>"; //
+							echo "<div class='visitar'>"; //
 							echo "<a href='anuncio.php?idAnuncio={$coluna['id']}'> VISITAR ANÚNCIO </a>";
 							echo "</div>"; //
-							echo "<div class='col-sm'>"; //
-							echo "</div>"; //
-							echo "<div class='col-sm'>"; //
+							echo "<div class='editar'>"; //
 							echo "<a href='editarAnuncio.php?idAnuncio={$coluna['id']}'> EDITAR ANÚNCIO </a>";
 							echo "</div>"; //
-							echo "<div class='col-sm'>"; //
+							echo "<div class='excluir'>"; //
 							echo "<a href='excluirAnuncio.php?idAnuncio={$coluna['id']}'> EXCLUIR ANÚNCIO </a>";
 							echo "</div>"; //
-							echo "</div>";
 							echo "</div>";
 						}
 
