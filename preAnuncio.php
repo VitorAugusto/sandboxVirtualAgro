c8a2a719de4141a6f43078c2a3a7acce3a5e96b7
MuriloGois <murilogois59@gmail.com>
2019-04-12 20:07:22 -0300

    Rio de Janeiro

--- preAnuncio.php
+++ preAnuncio.php
@@ -5,41 +5,18 @@ $categoria =  $_POST['categoria'];
 $produto = $_POST['produto'];
 $medida = $_POST['medida'];
 
-?>
+	echo "<div class='borda'> " . getImagemProduto(getIdProdutoPeloNome($produto)) . "</div>";
+	echo "<h1>".$produto."</h1>";
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-
-
-	<?php
-	echo "<ul class='list-group'>";
-	echo "<li class='list-group-item d-flex justify-content-between align-items-center'> " . getImagemProduto(getIdProdutoPeloNome($produto)) . "</li>";
-	echo "<li class='list-group-item d-flex justify-content-between align-items-center'> " . 
-	"<span class='badge badge-primary badge-pill' name='nomeProduto'>". $produto . "</span>".
-	"<span class='badge badge-primary badge-pill' name='nomeProduto'>". $medida . "</span>"
-	. "</li>";
-
-	echo "</ul>";
+	echo "<h4>R$ / ".$medida."</h4>" .
+	"<input type='text' name='valor' placeholder='PREÇO' onKeyPress='return(moeda(this,'.',',',event))' id='preco' required=''>".
+	"<h4>Mais algum detalhe?</h4>".
+	"<div class='form-group basic-textarea rounded-corners'>".
+	"<textarea class='form-control z-depth-1' id='obs' rows='3' name='observacao' placeholder='Detalhes...observações...'></textarea>".
+	"</div>".
+	"<input type='button' name='publicarButton' value='PUBLICAR' placeholder='PUBLICAR' class='btn btn-success' onclick='final()' id='botaoPublicar'>" .
+	"<input type='button' name='prev' class='prev acao' value='VOLTAR'>";
 
 	?>
-	<p></p>
 
 
-	<h5>Preço</h5>
-	R$<input type="text" name="valor" placeholder="PREÇO" onKeyPress="return(moeda(this,'.',',',event))" id="preco" required="">
-	<p></p>
-	<h6>Mais algum detalhe ?</h6>
-	<div class="form-group basic-textarea rounded-corners">
-		<textarea class="form-control z-depth-1" id="obs" rows="3" name="observacao" placeholder="Detalhes...observações..."></textarea>
-	</div>
-	<input type="button" name="publicarButton" value="PUBLICAR" placeholder="PUBLICAR" class="btn btn-success" onclick="final()" id="botaoPublicar"> 
-	<input type="button" name="prev" class="prev acao" value="VOLTAR">
-
-
-</body>
-
-</html>
